2ebd2af0c573295cf2046c8341309a6f8a73ae8f
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2020-03-23 14:09:21 +0100

    service working is serving!

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,27 @@
 var CACHE_NAME = "Cryptocurrency-1920";
-var urlsToCache = ["/img/", "/css/index.css", "/css/reset.css"];
+var urlsToCache = [
+  "/css/index.css",
+  "/img/background.jpg",
+  "/img/loading.svg",
+  "/img/icons/icon-144x144.png"
+];
 
 self.addEventListener("install", function(event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
     })
   );
 });
