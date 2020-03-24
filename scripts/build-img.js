30af93f1c15d91e506cf241b4e8f10dd841892fd
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2020-03-24 23:16:21 +0100

    set plugin settings

--- scripts/build-img.js
+++ scripts/build-img.js
@@ -2,6 +2,11 @@ const gulp = require("gulp");
 const image = require("gulp-imagemin");
 
 return gulp
-  .src("./src/img/icons/**")
-  .pipe(image())
-  .pipe(gulp.dest("./static/img/icons/"));
+  .src("./src/img/**")
+  .pipe(
+    image([
+      image.mozjpeg({ quality: 50, progressive: true }),
+      image.optipng({ optimizationLevel: 9 })
+    ])
+  )
+  .pipe(gulp.dest("./static/img"));
