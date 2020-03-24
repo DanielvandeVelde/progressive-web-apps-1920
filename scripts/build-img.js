5269ba2a930167aba4298eb75092575edc5390d9
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2020-03-24 22:52:26 +0100

    imagemin made image larger??? thus removed

--- scripts/build-img.js
+++ scripts/build-img.js
@@ -1,7 +1,3 @@
 const gulp = require("gulp");
-const image = require("gulp-imagemin");
 
-return gulp
-  .src("./src/img/**")
-  .pipe(image())
-  .pipe(gulp.dest("./static/img"));
+return gulp.src("./src/img/**").pipe(gulp.dest("./static/img"));
